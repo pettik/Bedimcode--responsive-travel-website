783cd421295f6224c74ee8c391ce33700c6cb1e2
PetrB <petrbednarski@gmail.com>
2025-07-30 16:45:21 +0200

    css styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER HOME ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
