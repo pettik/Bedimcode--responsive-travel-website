a0c004e4072eff921c2241d767743ff5d82407b2
pettik <petrbednarski@gmail.com>
2025-08-05 17:47:20 +0200

    next steps - js, css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,40 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /* Menu show */
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
 /* Menu hidden */
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== SWIPER HOME ===============*/
+const swiperHome = new Swiper('.home__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== SWIPER TESTIMONIAL ===============*/
