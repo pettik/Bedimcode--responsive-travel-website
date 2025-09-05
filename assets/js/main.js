364202e392360fb3bb9647581aa74b5780a861f9
PetrB <petrbednarski@gmail.com>
2025-09-05 17:31:03 +0200

    dark-theme done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,42 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /* Menu show */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
   });
 }
 
 /* Menu hidden */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
   });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
+  const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER HOME ===============*/
-const swiperHome = new Swiper('.home__swiper', {
+const swiperHome = new Swiper(".home__swiper", {
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   grabCursor: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   autoplay: {
@@ -46,23 +46,23 @@ const swiperHome = new Swiper('.home__swiper', {
 });
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const bgHeader = () => {
-  const header = document.getElementById('header');
+  const header = document.getElementById("header");
   this.scrollY >= 50
-    ? header.classList.add('bg-header')
-    : header.classList.remove('bg-header');
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
 };
-window.addEventListener('scroll', bgHeader);
+window.addEventListener("scroll", bgHeader);
 /*=============== SWIPER TESTIMONIAL ===============*/
-const swiperTestimonial = new Swiper('.testimonial__swiper', {
+const swiperTestimonial = new Swiper(".testimonial__swiper", {
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   spaceBetween: 48,
   grabCursor: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   autoplay: {
@@ -73,93 +73,93 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
 
 /*=============== ADD BLUR TO HEADER ===============*/
 const blurHeader = () => {
-  const header = document.getElementById('header');
+  const header = document.getElementById("header");
   // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
   this.scrollY >= 50
-    ? header.classList.add('blur-header')
-    : header.classList.remove('blur-header');
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
 };
-window.addEventListener('scroll', blurHeader);
+window.addEventListener("scroll", blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
   this.scrollY >= 350
-    ? scrollUp.classList.add('show-scroll')
-    : scrollUp.classList.remove('show-scroll');
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
 };
-window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
   const scrollY = window.pageYOffset;
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute('id'),
+      sectionId = current.getAttribute("id"),
       sectionsClass = document.querySelector(
-        '.nav__menu a[href*=' + sectionId + ']'
+        ".nav__menu a[href*=" + sectionId + "]"
       );
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add('active-link');
+      sectionsClass.classList.add("active-link");
     } else {
-      sectionsClass.classList.remove('active-link');
+      sectionsClass.classList.remove("active-link");
     }
   });
 };
-window.addEventListener('scroll', scrollActive);
+window.addEventListener("scroll", scrollActive);
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'ri-sun-line';
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-fill";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+  themeButton.classList.contains(iconTheme) ? "ri-moon-fill" : "ri-sun-fill";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](
+  themeButton.classList[selectedIcon === "ri-moon-fill" ? "add" : "remove"](
     iconTheme
   );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
+  origin: "top",
+  distance: "60px",
   duration: 3000,
   delay: 400,
   // reset: true // Animations repeat
 });
 
 sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`);
-sr.reveal(`.home__card`, { delay: 600, distance: '100px', interval: 100 });
-sr.reveal(`.about__data, .join__image`, { origin: 'right' });
-sr.reveal(`.about__image, .join__data`, { origin: 'left' });
+sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 100 });
+sr.reveal(`.about__data, .join__image`, { origin: "right" });
+sr.reveal(`.about__image, .join__data`, { origin: "left" });
 sr.reveal(`.popular__card`, { interval: 200 });
